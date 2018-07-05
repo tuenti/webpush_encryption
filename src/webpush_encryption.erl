fbd7c7f589a5e93792665222ca658b87983a90af
Jorge Diz Pico <jdiz@tuenti.com>
2018-07-05 11:37:59 +0200

    Adds decrypt/2, some style changes

--- src/webpush_encryption.erl
+++ src/webpush_encryption.erl
@@ -2,59 +2,87 @@
 %% @doc Web Push Payload Encryption.
 %%
 %% Based on:
-%%   https://developers.google.com/web/updates/2016/03/web-push-encryption and
-%%   https://github.com/GoogleChrome/web-push-encryption.
+%%   https://developers.google.com/web/updates/2016/03/web-push-encryption
+%%   https://github.com/GoogleChrome/web-push-encryption
 %%
 
 -module(webpush_encryption).
 
--define(AUTH_INFO, <<"Content-Encoding: auth", 0>>).
-
--type publicKey()  :: string()|binary().
--type authTokeny() :: string()|binary().
--type clientKey()  :: {publicKey(), authTokeny()}.
--type cryptData()  :: {binary(), binary(), binary()}.
+-type publicKey()  :: binary().
+-type privateKey() :: binary().
+-type authToken() :: binary().
+-type cipherText() :: binary().
+-type plainText() :: binary().
+-type salt() :: binary().
+-type clientKey()  :: {publicKey(), authToken()}.
+-type fullClientKeys() :: {publicKey(), privateKey(), authToken()}.
+-type cryptData()  :: {cipherText(), salt(), publicKey()}.
 
 -export_type([
-	publicKey/0, 
-	authTokeny/0, 
-	clientKey/0, 
+	publicKey/0,
+	privateKey/0,
+	authToken/0,
+	clientKey/0,
+	fullClientKeys/0,
 	cryptData/0
 ]).
 -export([
-	encrypt/3
+	encrypt/3,
+	decrypt/2
 ]).
 
 
--spec encrypt(binary(), clientKey(), non_neg_integer() ) -> cryptData().
-encrypt(Message, {ClientPublicKey, ClientAuthToken} = _ClientKey, PaddingLength) when 
-	is_list(ClientPublicKey), is_list(ClientAuthToken) ->
-	
-	encrypt(Message, {base64url:decode(ClientPublicKey), base64url:decode(ClientAuthToken)}, PaddingLength);
+%%%===================================================================
+%%% API
+%%%===================================================================
 
-encrypt(Message, {ClientPublicKey, ClientAuthToken} = _ClientKey, PaddingLength) ->
-	Salt = crypto:strong_rand_bytes(16),
-	ServerECDH = crypto:ec_curve(prime256v1),
-	{PublicKey, PrivateKey} = crypto:generate_key(ecdh, prime256v1),
-	SharedSecret = crypto:compute_key(ecdh, ClientPublicKey, PrivateKey, ServerECDH),
-	Prk = hkdf(ClientAuthToken, SharedSecret, ?AUTH_INFO, 32),
-	
-	Context = context(ClientPublicKey, PublicKey),
-	ContentEncryptionKeyInfo = info("aesgcm", Context),
-	ContentEncryptionKey = hkdf(Salt, Prk, ContentEncryptionKeyInfo, 16),
-	NonceInfo = info("nonce", Context),
-	Nonce = hkdf(Salt, Prk, NonceInfo, 12),
-	PaddingBuffer = create_padding(PaddingLength),
-
-	Plaintext = <<PaddingBuffer/binary, Message/binary>>,
+-spec encrypt(plainText(), clientKey(), non_neg_integer()) -> cryptData().
+encrypt(Message, {ClientPublicKey, ClientAuthToken}, PaddingLength) ->
+	{Salt, ServerPublicKey, ServerPrivateKey} = generate_server_keys(),
+	Ikm = ikm(ClientPublicKey, ServerPrivateKey, ClientAuthToken),
+	{ContentEncryptionKey, Nonce} = cek_and_nonce(ClientPublicKey, ServerPublicKey, Salt, Ikm),
+	Plaintext = add_padding(PaddingLength, Message),
 	Ciphertext = encrypt_payload(Plaintext, ContentEncryptionKey, Nonce),
-	{Ciphertext, Salt, PublicKey}.
+	{Ciphertext, Salt, ServerPublicKey}.
 
-% private
 
-hkdf(Salt, IKM, Info, Length) ->
+-spec decrypt(cryptData(), fullClientKeys()) -> plainText().
+decrypt({Ciphertext, Salt, ServerPublicKey}, {ClientPublicKey, ClientPrivateKey, ClientAuthToken}) ->
+	Ikm = ikm(ServerPublicKey, ClientPrivateKey, ClientAuthToken),
+	{ContentEncryptionKey, Nonce} = cek_and_nonce(ClientPublicKey, ServerPublicKey, Salt, Ikm),
+	PaddedMessage = decrypt_payload(Ciphertext, ContentEncryptionKey, Nonce),
+	remove_padding(PaddedMessage).
+
+
+%%%===================================================================
+%%% PRIVATE
+%%%===================================================================
+
+generate_server_keys() ->
+	Salt = crypto:strong_rand_bytes(16),
+	{ServerPublicKey, ServerPrivateKey} = crypto:generate_key(ecdh, prime256v1),
+	{Salt, ServerPublicKey, ServerPrivateKey}.
+
+
+ikm(TheirPublicKey, MyPrivateKey, ClientAuthToken) ->
+	Params = crypto:ec_curve(prime256v1),
+	SharedSecret = crypto:compute_key(ecdh, TheirPublicKey, MyPrivateKey, Params),
+	AuthInfo = info("auth", none),
+	hkdf(ClientAuthToken, SharedSecret, AuthInfo, 32).
+
+
+cek_and_nonce(ClientPublicKey, ServerPublicKey, Salt, Ikm) ->
+	Context = context(ClientPublicKey, ServerPublicKey),
+	ContentEncryptionKeyInfo = info("aesgcm", Context),
+	ContentEncryptionKey = hkdf(Salt, Ikm, ContentEncryptionKeyInfo, 16),
+	NonceInfo = info("nonce", Context),
+	Nonce = hkdf(Salt, Ikm, NonceInfo, 12),
+	{ContentEncryptionKey, Nonce}.
+
+
+hkdf(Salt, Ikm, Info, Length) ->
 	KeyHmac = crypto:hmac_init(sha256, Salt),
-	KeyHmac = crypto:hmac_update(KeyHmac, IKM),
+	KeyHmac = crypto:hmac_update(KeyHmac, Ikm),
 	Prk = crypto:hmac_final(KeyHmac),
 	
 	InfoHmac = crypto:hmac_init(sha256, Prk),
@@ -66,17 +94,50 @@ hkdf(Salt, IKM, Info, Length) ->
 context(ClientPublicKey, PublicKey) ->
 	<<0, (byte_size(ClientPublicKey)):16/big-unsigned-integer, ClientPublicKey/binary, (byte_size(PublicKey)):16/big-unsigned-integer, PublicKey/binary>>.
 
+
 info(Type, Context) when is_list(Type) ->
 	info(list_to_binary(Type), Context);
-	
+info(Type, none) ->
+	<<"Content-Encoding: ", Type/binary, 0>>;
 info(Type, Context) ->
-	<<"Content-Encoding: ", Type/binary, 0, "P-256", Context/binary>>.
-	
+	Encoding = info(Type, none),
+	<<Encoding/binary, "P-256", Context/binary>>.
+
+
+%%%===================================================================
+%%% PRIVATE CRYPTO
+%%%===================================================================
+
 encrypt_payload(Plaintext, ContentEncryptionKey, Nonce) ->
-	{CipherText, CipherTag} = crypto:block_encrypt(aes_gcm, ContentEncryptionKey, Nonce, {"", Plaintext}),
+	{CipherText, CipherTag} = crypto:block_encrypt(aes_gcm, ContentEncryptionKey, Nonce, {<<"">>, Plaintext}),
 	<<CipherText/binary, CipherTag/binary>>.
 
 
-create_padding(PaddingLength) ->
+decrypt_payload(Ciphertext, ContentEncryptionKey, Nonce) ->
+	{Text, Tag} = split_cipher(Ciphertext),
+	crypto:block_decrypt(aes_gcm, ContentEncryptionKey, Nonce, {<<"">>, Text, Tag}).
+
+
+split_cipher(Cipher) ->
+	<<Tag:16/binary, Text/binary>> = reverse(Cipher),
+	{reverse(Text), reverse(Tag)}.
+
+
+reverse(Bin) ->
+   Size = erlang:size(Bin)*8,
+   <<X:Size/integer-little>> = Bin,
+   <<X:Size/integer-big>>.
+
+
+%%%===================================================================
+%%% PRIVATE PADDING
+%%%===================================================================
+
+add_padding(PaddingLength, Message) ->
 	Length = <<PaddingLength:16/big-unsigned-integer>>,
-	<<Length/binary, (binary:copy(<<0>>, PaddingLength))/binary>>.
+	<<Length/binary, (binary:copy(<<0>>, PaddingLength))/binary, Message/binary>>.
+
+
+remove_padding(PaddedMessage) ->
+	<<PadLen:16/big-unsigned-integer, _Pad:PadLen/binary, PlainText/binary>> = PaddedMessage,
+	PlainText.
